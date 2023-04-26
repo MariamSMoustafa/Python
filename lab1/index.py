783962560420419b6a9485004c3e800420d321a7
Mariam <mariansaad585@gmail.com>
2023-04-26 19:58:24 +0200

    Day1

---
+++ lab1/index.py
@@ -0,0 +1,95 @@
+# first_name = input('Enter your First name:')
+# last_name = input('Enter your Last name:')
+
+# full_name = first_name + "" + last_name
+
+# print(full_name[::-1])
+
+###########################2##################################
+
+# n = int(input('Enter The number:'))
+# nn = n * 10 + n
+# nnn = n * 100 + nn
+# result =n + nn + nnn
+
+# print("n + nn + nnn =",result)
+
+###########################3##################################
+
+# print("""a string that you "don't" have to escape
+# This
+# is a ....... multi-line
+# heredoc string --------> example""")
+
+###########################4##################################
+
+# import math
+
+# r = 6
+# volume = (4/3) * math.pi * (r**3)
+
+# print("The volume of sphere is", volume)
+
+############################5#################################
+
+# b = int(input('Enter the base :'))
+# h = int(input('Enter the height :'))
+# Area = 1/2 * b * h
+
+# print("The Area of triangle is", Area)
+
+############################6#################################
+
+# n = 5
+# # print upper half of the pattern
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*",end =" ")
+#     print()
+
+# # print lower half of the pattern
+# for i in range(n - 1, 0, -1):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+############################7#################################
+
+# word = input('Enter your word:')
+
+# print(word[::-1])
+
+############################8#################################
+
+# for i in range(6):
+#     if i==3 or i==6:
+#         continue
+#     print(i)
+
+############################9#################################
+
+# n1, n2 = 0,1
+
+# print(n2, end=" ")
+
+# while n2 < 50:
+#     n3 = n1 + n2
+#     print(n3, end=" ")
+#     n1 = n2
+#     n2 = n3
+
+############################10#################################
+
+string = input('Enter The String:')
+
+num_digits = 0
+num_letters = 0
+
+for char in string:
+    if char.isdigit():
+        num_digits += 1
+    elif char.isalpha():
+        num_letters += 1
+
+print("Number of digits:", num_digits)
+print("Number of letters:", num_letters)
